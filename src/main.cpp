c745cb83edeb9b22326c2ac5f4bbdfb2f71cf948
Joshua <joshuasv1995@gmail.com>
2025-03-24 11:11:35 +0100

    -remove unused old function read_imu_data

--- src/main.cpp
+++ src/main.cpp
@@ -83,38 +83,6 @@ void detonate()
   digitalWrite(DETONATOR_GPIO_PIN, LOW);
 }
 
-void read_imu_data()
-{
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1A);
-  Wire.write(0x05);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1B);
-  Wire.write(0x8);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(0x68);
-  Wire.write(0x43);
-  Wire.endTransmission();
-
-  Wire.requestFrom(0x68,6);
-
-  int16_t GyroX=Wire.read()<<8 | Wire.read();
-  int16_t GyroY=Wire.read()<<8 | Wire.read();
-  int16_t GyroZ=Wire.read()<<8 | Wire.read();
-
-  float RateRoll=(float)GyroX/65.5;
-  float RatePitch=(float)GyroY/65.5;
-  float RateYaw=(float)GyroZ/65.5;
-
-  
-  Serial.println("IMU data:");
-  Serial.print("Roll rate [°/s]=");
-  Serial.println(RateRoll);
-}
-
 void setup()
 {
   #if DEBUG
@@ -143,6 +111,8 @@ void setup()
 void loop()
 {
   uint32_t current_time = millis();
+
+  Serial.println("State: " + state_to_str(state));
 
   switch (state)
   {
