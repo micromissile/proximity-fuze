b98511a9f223f3c1164bd5fadb2d871022113cb5
Joshua <joshuasv1995@gmail.com>
2025-06-19 13:02:30 +0200

    *modify timer value to 3s; payload ejection right after REVC burnout

--- include/Globals.h
+++ include/Globals.h
@@ -9,6 +9,6 @@
 // State machine global variables; format STATE_VAR_NAME
 #define IDLE_IMU_ACCEL_IS_MOVING_THR 1.0 // Acceleration threshold in m/s^2
 #define IDLE_IMU_IS_MOVING_PERSISTENT_COUNT 5 // Number of consecutive readings to confirm movement
-#define DETONATOR_TIMER_MS 5000
+#define DETONATOR_TIMER_MS 3000
 #define DETONATOR_ACTIVE_TIME_MS 5000
 #define FAILSAFE_TIMEOUT_MS 15 * 60 * 1000 // 15 minutes in milliseconds
